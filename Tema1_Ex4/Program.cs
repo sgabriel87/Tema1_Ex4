19c2f8242323f7ec18047ddb376ec5168c51ba7c
Gabriel <Gabriel@GABRIEL>
2024-02-18 19:59:38 +0200

    Add project files.

---
+++ Tema1_Ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿/*EX4
+Scrieti un program care va afisa semnul unui numar citit de la tastatura
+• Daca numarul este pozitiv, va afisa “+”
+• Daca numarul este negativ, va afisa “-”
+• Daca numarul este 0, va afisa “0”*/
+
+
+    Console.WriteLine("Introduceti un numar:");
+    // Citirea numărului de la tastatură și convertirea într-un număr întreg
+    int numar = Convert.ToInt32(Console.ReadLine());
+
+    // Verificarea semnului
+    if (numar > 0)
+    {
+        Console.WriteLine("+");
+    }
+    else if (numar < 0)
+    {
+        Console.WriteLine("-");
+    }
+    else
+    {
+        Console.WriteLine("0");
+    }
+
